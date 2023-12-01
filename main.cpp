a28200a3220e4348105b8a2c6e9e1d6b24658e1b
Fabian <fizquierdoo@est.ups.edu.ec>
2023-12-01 03:14:48 -0500

    Proyect complete

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include "interfaz.h"
+#include "prosimples.h"
+#include "provariable.h"
 
 using namespace std;
 
 int main()
 {
     Interfaz itz;
+    Modelo* proSimple1 = new proSimples("SKU001", "Whiskey", "Para un buen paladar", 29.99, 15.60, 50);
+    Modelo* proSimple2 = new proSimples("SK002", "Tropico", "Un buen licor", 10.99, 5.56, 20);
+    Inventario inv;
+    inv.agregarProducto(proSimple1);
+    inv.agregarProducto(proSimple2);
+    Modelo* proVariado1 = new proVariable("SK003", "Jonny Walker", "Un elixir", 99.99, 70.42, 32, 2, "Blue Label");
+    Modelo* proVariado2 = new proVariable("SK004", "Zhumir", "Agua ardiente", 30.32, 20.66, 10, 2, "Zhumir sandia");
+    inv.agregarProducto(proVariado1);
+    inv.agregarProducto(proVariado2);
+    inv.cuadroProductos();
+    inv.cuadroInventario();
+    inv.calcularCosto("SKU001", 18.50, 10);
+    inv.calcularCosto("SKU003", 6.25, 8);
+    inv.cuadroProductos();
+    inv.calculoEgreso("SKU002", 5);
+    inv.calculoEgreso("SKU004", 3);
+    inv.cuadroInventario();
     int opcion;
     system("color A");
     do
     {
-        cout << "\n== = == Menu de Inventario == = ==" << endl;
+        cout << "\n== = == API Ales Tavern == = ==" << endl;
         cout << "1. Agregar Producto" << endl;
         cout << "2. Mostrar Cuadro de Productos" << endl;
         cout << "3. Mostrar Cuadro de Inventario" << endl;
@@ -20,8 +39,10 @@ int main()
         cout << "*******************************" << endl;
         cout << "Ingrese la opcion: ";
         cin >> opcion;
-        switch (opcion) {
+        switch (opcion)
+        {
         case 1:
+
             itz.agregarProducto();
             system("cls");
             break;
@@ -39,6 +60,12 @@ int main()
             break;
         case 6:
             cout << "Gracias por usar el programa..." << endl;
+            cout << "= == Integrantes == =" << endl;
+            cout << "1. Ariel Flores" << endl;
+            cout << "2. Mayerli Campuez" << endl;
+            cout << "3. Luis Pillajo" << endl;
+            cout << "4. Fabian Izquierdo" << endl;
+            cout << "===== = === = ====="<<endl;
             break;
         default:
             cout << "Opcion no valida, ingrese de nuevo..." << endl;
